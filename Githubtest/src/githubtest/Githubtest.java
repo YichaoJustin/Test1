ab0a41e700086c92c1c1bbcaff0f8a3ae83bf141
YichaoJustin <YichaoJustin@10.111.91.146>
2014-04-30 12:29:14 +0100

    test

---
+++ Githubtest/src/githubtest/Githubtest.java
@@ -0,0 +1,17 @@
+package githubtest;
+
+import processing.core.PApplet;
+
+
+public class Githubtest extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { githubtest.Githubtest.class.getName() });
+	}
+}
